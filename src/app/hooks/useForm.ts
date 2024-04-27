b3e397b6aebcb736274bb9b89fe9ceb76b7d30e7
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-27 12:18:12 -0500

    WIP useForm hook

--- src/app/hooks/useForm.ts
+++ src/app/hooks/useForm.ts
@@ -1,14 +1,50 @@
 import { useState } from "react";
 
-export const useForm = <T>(defaultValue: T): [T, (property: string) => (e: React.FormEvent<HTMLInputElement>) => void]  => {
-  const [model, setModel] = useState(defaultValue);
+export type PropertyStatus = { status: boolean, message?: string}
+type DicOfProperties<Type> = {
+  [Property in keyof Type]: PropertyStatus;
+};
+export type ValueType = string | undefined | null;
+type ReturnType<T> = [
+  T, 
+  (property: string) => (e: React.FormEvent<HTMLInputElement>) => void,
+  () => boolean
+];
+export type FunctionTable = {[key: string]: (value: ValueType) => PropertyStatus};
 
-  const handleChange = (property: string) => (e: React.FormEvent<HTMLInputElement>) => {
+export const useForm = <T>(defaultValue: T, checkFunctions?: FunctionTable): ReturnType<T> => {
+  const [model, setModel] = useState(defaultValue);
+  const [modelStatus, setModelStatus] = useState({} as DicOfProperties<T>)
+
+  const handleChange = (property: string) => (e: React.FormEvent<HTMLInputElement>) => {    
     setModel({
       ...model,
       [property]: e.currentTarget.value
     })
+    // Check if the current property has any check
+    if (checkFunctions && property in checkFunctions) {
+      type KeyofT  = keyof T;
+      setModelStatus({
+        ...modelStatus,
+        [property]: checkFunctions[property](e.currentTarget.value as string)
+      })
+    }
+      
   }
 
-  return [model, handleChange];
+  // Function to check if the model is okay to be dispatch
+  const checkModel = () => {
+    let property: keyof T;
+    console.log("checking...")
+    for (property in modelStatus) {      
+      console.log(`${property as string}: ${modelStatus[property]}` )
+      if (!modelStatus[property]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  
+  return [model, handleChange, checkModel];
 }
