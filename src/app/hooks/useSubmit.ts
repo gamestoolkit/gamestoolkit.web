7334dfb6254e105782f64156774b6250007068bf
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-27 12:18:24 -0500

    WIP useSubmit hook

--- src/app/hooks/useSubmit.ts
+++ src/app/hooks/useSubmit.ts
@@ -1,8 +1,14 @@
 import { useRouter } from "next/navigation";
 
-export const useSubmit = <T>(effect: (m: T) => Promise<void>, redirectTo?: string) => {
+export const useSubmit = <T>(effect: (m: T) => Promise<void>, checkModel?: () =>boolean, redirectTo?: string) => {
   const router = useRouter();  
   const onSubmit = (model: T) => async (e: React.FormEvent<HTMLFormElement>) => {
+    if (checkModel !== undefined) {
+      if (!checkModel()) {
+        alert("Some fields are required");
+        return;
+      }
+    }
     e.preventDefault();
     await effect(model);
     if (redirectTo !== undefined) {
