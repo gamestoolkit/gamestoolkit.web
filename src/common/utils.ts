d48888b44fa9fd49beb2f6360d2f8f4e949e2851
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-27 12:17:40 -0500

    Added maybeFunction

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -7,3 +7,15 @@ export async function maybePromise<T>(func: () => Promise<T>, defaultValue: T) :
   }
 }
 
+
+export function maybeFunction<T>(func: () => Promise<T>, defaultValue: T) : () => Promise<T> {
+  return async function () {
+    try {
+      return await func();
+    } catch (error) {
+      console.error(error);
+      return defaultValue;
+    }
+  }  
+}
+
